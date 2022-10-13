fe9ec7f7eed7133e65be940e8f1c2106b79d5ecc
ZARIFMAC <uzbzarif@gmail.com>
2022-10-13 15:41:10 +0500

    Todolist

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:todo_beginner/screen/todoList.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Container(),
+    home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: TodoList()),
   ));
 }
