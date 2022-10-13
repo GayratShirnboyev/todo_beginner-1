a74002c2f9e45c644ca6f9e7e8a01cc948a3280f
ZARIFMAC <uzbzarif@gmail.com>
2022-10-13 15:26:02 +0500

    todoList class rename to todo

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,7 +1,6 @@
-class TodoList {
+class Todo {
   final String title;
   final String description;
   final bool status;
-  TodoList(
-      {required this.title, required this.description, required this.status});
+  Todo({required this.title, required this.description, required this.status});
 }
