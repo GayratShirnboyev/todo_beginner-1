c1b75c7f1193c030a77b6e9388fff6136634e1dc
ZARIFMAC <uzbzarif@gmail.com>
2022-10-13 15:43:21 +0500

    Floating Action Button

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:todo_beginner/screen/todoList.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-        appBar: AppBar(
-          title: Text('My App'),
-        ),
-        body: TodoList()),
-  ));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Todo List'),
+          ),
+          body: TodoList(),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print(1);
+            },
+            child: const Icon(Icons.add),
+          )),
+    ),
+  );
 }
