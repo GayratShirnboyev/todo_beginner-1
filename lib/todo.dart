d71ffc333a863f75d2075cfc26aa996e994d8c4c
ZARIFMAC <uzbzarif@gmail.com>
2022-10-13 15:22:11 +0500

    todo class

---
+++ lib/todo.dart
@@ -0,0 +1,7 @@
+class TodoList {
+  final String title;
+  final String description;
+  final bool status;
+  TodoList(
+      {required this.title, required this.description, required this.status});
+}
