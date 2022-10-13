045a3113e5dadf384bc683024b51f565693de778
ZARIFMAC <uzbzarif@gmail.com>
2022-10-13 15:32:42 +0500

    TodoList

---
+++ lib/screen/todoList.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../todo.dart';
+
+class TodoList extends StatefulWidget {
+  TodoList({Key? key}) : super(key: key);
+  List<Todo> todoList = [
+    Todo(title: 'Todo 1', description: 'Description 1', status: false),
+    Todo(title: 'Todo 2', description: 'Description 2', status: false),
+  ];
+
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: widget.todoList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(widget.todoList[index].title),
+            subtitle: Text(widget.todoList[index].description),
+            trailing: Checkbox(
+              value: widget.todoList[index].status,
+              onChanged: (value) {},
+            ),
+          );
+        });
+  }
+}
